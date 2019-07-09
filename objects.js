7ef3a875ba36983c7bd2e0946a05eeebe1ea14f7
amir shariff <amirshariff@msn.com>
2019-07-09 12:59:52 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 let playlist = {artist:'john'}
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist = object.assign()
-  playlist.push({artistName:songTitle})
+  playlist = object.assign({artistName:songTitle},playlist)
+  
 }
